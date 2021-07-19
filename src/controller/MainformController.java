a7e36f3a51f4d95aa84ca2e0ed3dab6d3312055e
ChandimaHerath <chandimaherath@hotmail.com>
2021-07-19 23:09:58 +0530

    model and Controller implemented

--- src/controller/MainformController.java
+++ src/controller/MainformController.java
@@ -1,4 +1,37 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import model.SubjectMarks;
+
 public class MainformController {
+    public TextField txtID;
+    public TextField txtName;
+    public TextField txtSubject;
+    public TextField txtMarks;
+    public Button btnRemove;
+    public Button btnAdd;
+    public Button btnPrint;
+    public Button btnExport;
+    public TableView<SubjectMarks> tblMarks;
+
+
+    public void initialize(){
+
+
+    }
+
+    public void btnRemove_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAdd_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPrint_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnExport_OnAction(ActionEvent actionEvent) {
+    }
 }
